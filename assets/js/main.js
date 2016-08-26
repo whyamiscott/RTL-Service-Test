6ce6631803b2add55f40736dbdf3a9c1671063ef
whyamiscott <whyamiscott@yandex.ru>
2016-08-26 21:51:50 +0800

    devices panel added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,96 @@
 var ws = new WebSocket('ws://localhost:8081');
 
+function DeviceView(isOnline, name) {
+	this.isOnline = isOnline;
+	this.name = name;
+
+	var that = this;
+
+	this.setIsOnline = function(val) {
+		if (val) {
+			setInnerHTML(that.isOnline, 'On-line');
+		} else {
+			setInnerHTML(that.isOnline, 'Off-line');
+		}
+	}
+
+	this.setName = function(val) {
+		setInnerHTML(that.name, val);
+	}
+
+	this.update = function(properties) {
+		if (properties.hasOwnProperty('isOnline')) this.setIsOnline(properties.isOnline);
+		if (properties.hasOwnProperty('name')) this.setName(properties.name);
+	}
+
+	function setInnerHTML(elem, html) {
+		elem.innerHTML = html;
+	}
+}
+
+function DeviceController(deviceModel, deviceView) {
+	this.model = deviceModel;
+	this.view = deviceView;
+
+	this.model.attachObserver(this.view);
+
+	this.view.setIsOnline(this.model.getStatus().isOnline);
+	this.view.setName(this.model.getStatus().name);
+}
+
+function DeviceModel(mac, location, status) {
+	var mac = mac;
+	var location = location;
+	var status = status;
+	var observers = [];
+
+	this.getStatus = function() {
+		return status;
+	}
+
+	this.getLocation = function() {
+		return location;
+	}
+
+	this.attachObserver = function(o) {
+		observers.push(o);
+	}
+
+	function notifyObservers(properties) {
+		observers.forEach(function(o) {
+			o.update(properties);
+		})
+	}
+
+	this.setStatus = function(newStatus) {
+		var changed = [],
+			properties = {},
+			i = 0;
+
+		if (!newStatus.hasOwnProperty('isOnline') && !newStatus.hasOwnProperty('name')) return false;
+
+		if (status.isOnline != newStatus.isOnline) {
+			status.isOnline = newStatus.isOnline;
+			changed.push('isOnline');
+		}
+
+		if (status.name != newStatus.name) {
+			status.name = newStatus.name;
+			changed.push('name');
+		}
+
+		for (i; i<changed.length; i++) {
+			properties[changed[i]] = newStatus[changed[i]];
+		}
+
+		notifyObservers(properties);
+
+		return true;
+	}
+}
+
+var devices = {};
+
 ws.onopen = function() {
 	console.log('Соединение установлено!');
 };
@@ -7,12 +98,37 @@ ws.onopen = function() {
 ws.onmessage = function(event) {
 	var data = JSON.parse(event.data);
 
+	if (!devices.hasOwnProperty(data.mac) && data.type != 'device') return false;
+
 	switch (data.type) {
 		case 'location':
-			console.log('LOCATION:', event.data);
+			//console.log('LOCATION:', event.data);
 			break;
 		case 'status':
-			console.log('STATUS:', event.data);
+			devices[data.mac].model.setStatus(data.status);
+			break;
+		case 'device':
+			var model = new DeviceModel(data.mac, data.location, data.status),
+				view,
+				controller,
+				deviceHTMLNode = document.createElement('div'),
+				isOnlineHTMLNode = document.createElement('p'),
+				nameHTMLNode = document.createElement('p');
+
+			deviceHTMLNode.id = 'device_' + data.mac;
+			deviceHTMLNode.className = 'device';
+			isOnlineHTMLNode.id = deviceHTMLNode.id + '__isOnline';
+			nameHTMLNode.id = deviceHTMLNode.id + '__name';
+
+			deviceHTMLNode.appendChild(isOnlineHTMLNode);
+			deviceHTMLNode.appendChild(nameHTMLNode);
+			document.getElementById('devices').appendChild(deviceHTMLNode);
+
+			view = new DeviceView(isOnlineHTMLNode, nameHTMLNode);
+			controller = new DeviceController(model, view);
+
+			devices[data.mac] = {model: model, view: view, controller: controller};
+
 			break;
 		default:
 			console.log('Unknown type')
--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(express.static('assets'));
 app.use(bodyParser.json());
 
 class Device {
-	constructor(deviceKey, isOnline, locationGenerationFreg, statusGenerationFreg) {
+	constructor(deviceKey, locationGenerationFreg, statusGenerationFreg) {
 		this.deviceKey = deviceKey;
-		this.isOnline = isOnline | false;
+		this.status = {isOnline: false, name: 0};
 		this.location = {
 			lon: Math.round(Math.random() * 1) ? Math.random() * 180 : Math.random() * -180,
 			lat: Math.round(Math.random() * 1) ? Math.random() * 90 : Math.random() * -90,
@@ -26,52 +26,100 @@ class Device {
 		this.statusGenerationFreg = statusGenerationFreg | 1000;
 		this.locationGenerationError = Math.round(Math.round(Math.random() * 1) ? Math.random() * 50 : Math.random() * -50);
 		this.statusGenerationError = Math.round(Math.round(Math.random() * 1) ? Math.random() * 50 : Math.random() * -50);
+
+		setInterval(this.randomChangeStatus, 5000, this);
 	}
 
-	setOnline() {this.online = true}
-	setOffline() {this.online = false}
+	randomChangeStatus(that) {
+		let changeIsOnline = Math.round(Math.random() * 1),
+			changeName = Math.round(Math.random() * 1);
+
+		if (changeIsOnline) {
+			that.status.isOnline = Math.round(Math.random() * 1) ? true : false;
+		}
+
+		if (changeName) {
+			that.status.name = Math.round(Math.random() * 42);
+		}
+	}
+
+	setIntervalForMethod(method, time) {
+		setInterval(method, time);
+	}
 
 	getLocationPacket() {
+		let location = this.location;
+
 		return {
 			mac: this.deviceKey,
 			type: 'location',
 			ts: Date.now(),
-			location: this.location
+			location: location
 		}
 	}
 
 	getStatusPacket() {
+		let status = this.status;
+
 		return {
 			mac: this.deviceKey,
 			type: 'status',
 			ts: Date.now(),
-			status: {isOnline: this.isOnline, name: Math.random()}
+			status: status
+		}
+	}
+
+	getDevicePacket() {
+		let status = this.status;
+		let location = this.location;
+
+		return {
+			mac: this.deviceKey,
+			type: 'device',
+			status: status,
+			location: location
 		}
 	}
 }
 
-let myDevice = new Device(232139);
+
+let devices = [];
+const N = 1000;
+
+for (let i=0; i<N; i++) {
+	devices.push(new Device(i));
+}
 
 wsServer.on('connection', (ws) => {
-	let locationTimer = setInterval(
-		wsSendMsgOnTimer,
-		myDevice.locationGenerationFreg + myDevice.locationGenerationError,
-		ws,
-		myDevice,
-		'location'
-	);
+	let timers = [];
 
-	let statusTimer = setInterval(
-		wsSendMsgOnTimer,
-		myDevice.statusGenerationFreg + myDevice.statusGenerationError,
-		ws,
-		myDevice,
-		'status'
-	);
+	for (let i=0; i<devices.length; i++) {
+		wsSendMsg(ws, devices[i], 'device');
+
+		let locationTimer = setInterval(
+			wsSendMsg,
+			devices[i].locationGenerationFreg + devices[i].locationGenerationError,
+			ws,
+			devices[i],
+			'location'
+		);
+
+		let statusTimer = setInterval(
+			wsSendMsg,
+			devices[i].statusGenerationFreg + devices[i].statusGenerationError,
+			ws,
+			devices[i],
+			'status'
+		);
+
+		timers.push(locationTimer);
+		timers.push(statusTimer);
+	}
 
 	ws.on('close', () => {
-		clearInterval(locationTimer);
-		clearInterval(statusTimer);
+		for (let i=0; i<timers.length; i++) {
+			clearInterval(timers[i]);
+		}
 	})
 
 	ws.on('message', (data, flags) => {
@@ -79,13 +127,16 @@ wsServer.on('connection', (ws) => {
 	});
 });
 
-function wsSendMsgOnTimer(ws, device, type) {
+function wsSendMsg(ws, device, type) {
 	switch (type) {
 		case 'location':
 			ws.send(JSON.stringify(device.getLocationPacket()));
 			break;
 		case 'status':
 			ws.send(JSON.stringify(device.getStatusPacket()));
+			break;
+		case 'device':
+			ws.send(JSON.stringify(device.getDevicePacket()));
 			break;
 		default:
 			console.log('Unknown type of packet.');
